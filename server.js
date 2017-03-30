50031fd70c76b4dc6b7773435b4fcaab01ecb53f
Soós Ferenc <pkferi@gmail.com>
2017-03-31 01:22:32 +0200

    initial program

---
+++ server.js
@@ -0,0 +1,204 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+const uuid = require('uuid/v4');
+
+const SERVER_PORT = 8080;
+const DEBUG = true;
+
+var server = http.createServer(function (request, response) {
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    if (filePath == './favicon.ico')
+        filePath = './favicons/favicon-0.ico';
+
+    if (filePath == './server.js') {
+        response.writeHead(403);
+        response.end('Noope');
+        return;
+    }
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.ico':
+            contentType = 'image/x-icon';
+            break;
+    }
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.writeHead(200, {'Content-Type': contentType});
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
+    });
+
+});
+
+server.listen(SERVER_PORT, function() {
+    console.log('Server listening on port: ' + SERVER_PORT);
+});
+
+var wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+var clients = {};
+var subscribers = [];
+
+function sendToAll(obj) {
+    var msg = JSON.stringify(obj);
+    for (var key in clients) {
+        if (!clients.hasOwnProperty(key)) continue;
+        var client = clients[key];
+        client.connection.sendUTF(msg);
+    }
+}
+
+function sendToConnection(connection, obj) {
+    connection.sendUTF(JSON.stringify(obj));
+}
+
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+    connection.uuid = uuid();
+    clients[connection.uuid] = {
+        connection: connection,
+        name: null
+    };
+    if (DEBUG) console.log('Client connected with uuid: ' + connection.uuid);
+
+    for (var key in clients) {
+        if (!clients.hasOwnProperty(key)) continue;
+        if (key == connection.uuid) continue;
+        if (clients[key].name === null) continue;
+        var client = clients[key];
+        sendToConnection(connection, {
+            pid: 'player-connected',
+            uuid: client.connection.uuid,
+            name: client.name
+        });
+    }
+    for (var i = 0; i < subscribers.length; i++) {
+        var client = clients[subscribers[i]];
+        sendToAll({
+            pid: 'player-subscribed',
+            uuid: client.connection.uuid,
+            name: client.name,
+            amount: subscribers.length
+        });
+    }
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            try {
+                var obj = JSON.parse(message.utf8Data);
+                if (obj.pid === undefined) return;
+                if (obj.pid == 'name') {
+                    if (clients[connection.uuid].name === null) {
+                        sendToConnection(connection, {
+                            pid: 'player-accepted'
+                        });
+                    }
+                    clients[connection.uuid].name = obj.name;
+                    sendToAll({
+                        pid: 'player-connected',
+                        uuid: connection.uuid,
+                        name: obj.name
+                    });
+                } else if (obj.pid == 'subscribe') {
+                    if (clients[connection.uuid].name === null) return;
+                    if (subscribers.indexOf(connection.uuid) !== -1) return;
+
+                    subscribers.push(connection.uuid);
+                    if (subscribers.length < 4) {
+                        sendToConnection(connection, {
+                            pid: 'subscribe-accepted'
+                        });
+                        sendToAll({
+                            pid: 'player-subscribed',
+                            uuid: connection.uuid,
+                            name: clients[connection.uuid].name,
+                            amount: subscribers.length
+                        });
+                    } else {
+                        // GAME START
+                        var names = [];
+                        var i;
+                        for (i = 0; i < subscribers.length; i++) {
+                            names.push(clients[subscribers[i]].name);
+                        }
+                        names = names.join(', ');
+                        for (i = 0; i < subscribers.length; i++) {
+                            sendToConnection(clients[subscribers[i]].connection, {
+                                pid: 'game',
+                                notification: names
+                            });
+                        }
+                        sendToAll({
+                            pid: 'clear-subscribe'
+                        });
+                        subscribers = [];
+                    }
+                } else if (obj.pid == 'unsubscribe') {
+                    var index = subscribers.indexOf(connection.uuid);
+                    if (index === -1) return;
+                    subscribers.splice(index, 1);
+                    sendToConnection(connection, {
+                        pid: 'unsubscribe-accepted'
+                    });
+                    sendToAll({
+                        pid: 'player-unsubscribed',
+                        uuid: connection.uuid,
+                        amount: subscribers.length
+                    });
+                }
+            } catch (e) {}
+        }
+    });
+
+    connection.on('close', function() {
+        // close user connection
+        var i = subscribers.indexOf(connection.uuid);
+        if (i !== -1)
+            subscribers.splice(i, 1);
+        delete clients[connection.uuid];
+        if (DEBUG) console.log('Client disconnected with uuid: ' + connection.uuid);
+        sendToAll({
+            pid: 'player-disconnected',
+            uuid: connection.uuid,
+            amount: subscribers.length
+        });
+    });
+});
---
+++ soccribe.js
@@ -0,0 +1,161 @@
+(function () {
+
+    var onlyAlert = false;
+    var ws;
+
+    var playerName;
+    var setName;
+    var subscribe;
+    var unsubscribe;
+    var subscribedList;
+    var availablePlayers;
+
+    function changeFavicon(number) {
+        $('#dynamic-favicon').attr('href', 'favicons/favicon-' + +number + '.ico');
+        $('#dynamic-favicon-32').attr('href', 'favicons/favicon-' + +number + '-32x32.png');
+        $('#dynamic-favicon-16').attr('href', 'favicons/favicon-' + +number + '-16x16.png');
+    }
+
+    function connect() {
+        ws = new WebSocket(window.location.href.replace('http', 'ws'));
+        ws.onopen = onOpen;
+        ws.onerror = onError;
+        ws.onclose = onClose;
+        ws.onmessage = onMessage;
+    }
+
+    function send(obj) {
+        ws.send(JSON.stringify(obj));
+    }
+
+    function onOpen() {
+        console.log('opened');
+        playerName.prop('disabled', false);
+        setName.removeClass('disabled');
+        playerName.focus();
+    }
+
+    function onError() {
+        console.log('error');
+    }
+
+    function onClose() {
+        console.log('close');
+        // reconnect?
+        playerName.prop('disabled', true);
+        setName.addClass('disabled');
+        subscribe.addClass('disabled');
+        unsubscribe.addClass('disabled');
+        subscribedList.html('');
+        availablePlayers.html('');
+    }
+
+    function onMessage(msg) {
+        try {
+            var data = JSON.parse(msg.data);
+            console.log(data);
+            if (data.pid == 'player-connected') {
+                var player = $('.' + data.uuid);
+                if (player.length) {
+                    player.html(data.name);
+                } else {
+                    var html = $('<li class="list-group-item"></li>');
+                    html.addClass(data.uuid);
+                    var escaped = $("<div>").text(data.name).html();
+                    html.html(escaped);
+                    html.appendTo(availablePlayers);
+                }
+            } else if (data.pid == 'player-accepted') {
+                subscribe.removeClass('disabled');
+            } else if (data.pid == 'player-disconnected') {
+                $('.' + data.uuid).remove();
+                changeFavicon(data.amount);
+            } else if (data.pid == 'player-subscribed') {
+                var player = subscribedList.find('.' + data.uuid);
+                if (player.length) {
+                    player.html(data.name);
+                } else {
+                    var html = $('<li class="list-group-item"></li>');
+                    html.addClass(data.uuid);
+                    var escaped = $("<div>").text(data.name).html();
+                    html.html(escaped);
+                    html.appendTo(subscribedList);
+                }
+                changeFavicon(data.amount);
+            } else if (data.pid == 'subscribe-accepted') {
+                subscribe.addClass('disabled');
+                unsubscribe.removeClass('disabled');
+            } else if (data.pid == 'player-unsubscribed') {
+                subscribedList.find('.' + data.uuid).remove();
+                changeFavicon(data.amount);
+            } else if (data.pid == 'unsubscribe-accepted') {
+                subscribe.removeClass('disabled');
+                unsubscribe.addClass('disabled');
+            } else if (data.pid == 'game') {
+                if (onlyAlert || Notification.permission !== "granted") {
+                    alert("GAME: " + data.notification);
+                } else {
+                    var notification = new Notification("Game!", {
+                        body: data.notification,
+                        icon: window.location.href + 'soccer-court.png'
+                    });
+                    notification.onclick = function() {
+                        window.focus();
+                        notification.close();
+                    };
+                }
+                subscribe.removeClass('disabled');
+                unsubscribe.addClass('disabled');
+            } else if (data.pid == 'clear-subscribe') {
+                subscribedList.find('li').remove();
+                changeFavicon(0);
+            }
+        } catch (e) {}
+    }
+
+    $(function () {
+        // READY
+        if (!Notification) {
+            onlyAlert = true;
+        } else if (Notification.permission !== "granted") {
+            Notification.requestPermission();
+        }
+
+        playerName = $('#player-name');
+        setName = $('#set-name');
+        subscribe = $('#subscribe');
+        unsubscribe = $('#unsubscribe');
+        subscribedList = $('#subscribed-list');
+        availablePlayers = $('#available-players');
+
+        playerName.keyup(function(e) {
+            if ((e.keyCode | e.which) === 13 ) {
+                setName.click();
+            }
+        });
+
+        setName.click(function() {
+            var name = playerName.val().trim();
+            if (name.length === 0) return;
+            send({
+                pid: 'name',
+                name: name
+            });
+        });
+
+        subscribe.click(function() {
+            send({
+                pid: 'subscribe'
+            });
+        });
+
+        unsubscribe.click(function() {
+            send({
+                pid: 'unsubscribe'
+            });
+        });
+
+        //websocket connect
+        connect();
+    });
+})();
